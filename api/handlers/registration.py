40f9a6dec35d36e3e3889ca86c368962d8d97690
HaldenLF <Liamhalliden20@gmail.com>
2026-04-27 10:48:14 +0800

    added security features to registration test & api files

--- api/handlers/registration.py
+++ api/handlers/registration.py
@@ -1,6 +1,8 @@
+import os
+import json
 from tornado.escape import json_decode
-
 from .base import BaseHandler
+from .sec_utils import get_encryption_key, hash_passphrase, encrypt_data
 
 class RegistrationHandler(BaseHandler):
 
@@ -10,10 +12,16 @@ class RegistrationHandler(BaseHandler):
             email = body['email'].lower().strip()
             password = body['password']
             display_name = body.get('displayName')
+            address = body.get('address')
+            date_of_birth = body.get('dateOfBirth')
+            phone_number = body.get('phoneNumber')
+            disabilities = body.get('listOfDisabilities', [])
+            
             if display_name is None:
                 display_name = email
             if not isinstance(display_name, str):
                 raise Exception('Display name must be a string')
+            
         except Exception:
             self.send_error(400, message='You must provide an email address, password and display name!')
             return
@@ -31,21 +39,42 @@ class RegistrationHandler(BaseHandler):
             return
 
         user = await self.db.users.find_one({
-          'email': email
+            'email': email
         })
 
         if user is not None:
             self.send_error(409, message='A user with the given email address already exists!')
             return
 
+        salt = os.urandom(32)
+        password_hash = hash_passphrase(password, salt)
+        
+        personal_info = {
+            'display_name': display_name,
+            'address': address,
+            'dateOfBirth': date_of_birth,
+            'phoneNumber': phone_number,
+            'listOfDisabilities': disabilities
+        }
+        
+        personal_info_str = json.dumps(personal_info)
+        
+        # Encrypting data
+        encryption_key = get_encryption_key()
+        encrypted_info = encrypt_data(personal_info_str, encryption_key)
+
+        # Store data
         await self.db.users.insert_one({
             'email': email,
-            'password': password,
-            'displayName': display_name
+            'password_hash': password_hash,
+            'salt': salt,
+            'encrypted_personal_info': encrypted_info,
+            'token_hash': None,
+            'expiresIn': None
         })
 
         self.set_status(200)
         self.response['email'] = email
         self.response['displayName'] = display_name
-
+        # self.response
         self.write_json()
--- test/registration.py
+++ test/registration.py
@@ -1,9 +1,11 @@
+import json
 from json import dumps
 from tornado.escape import json_decode
 from tornado.ioloop import IOLoop
 from tornado.web import Application
 
 from api.handlers.registration import RegistrationHandler
+from api.handlers.sec_utils import get_encryption_key, check_passphrase, decrypt_data
 
 from .base import BaseTest
 
@@ -19,10 +21,11 @@ class RegistrationHandlerTest(BaseTest):
     def test_registration(self):
         email = 'test@test.com'
         display_name = 'testDisplayName'
+        password = 'testPassword'
 
         body = {
           'email': email,
-          'password': 'testPassword',
+          'password': password,
           'displayName': display_name
         }
 
@@ -32,6 +35,30 @@ class RegistrationHandlerTest(BaseTest):
         body_2 = json_decode(response.body)
         self.assertEqual(email, body_2['email'])
         self.assertEqual(display_name, body_2['displayName'])
+        
+        # Check that user is stored in database
+        user = IOLoop.current().run_sync(
+          lambda: self.get_app().db.users.find_one({'email': email})
+        )
+        
+        # Check that password not stored in plaintext and salt and hash are stored in database
+        self.assertIsNone(user.get('password'), 'Password should not be stored in plaintext')
+        self.assertIsNotNone(user.get('password_hash'), 'Password hash should be stored')
+        self.assertIsNotNone(user.get('salt'), 'Salt should be stored')
+        
+        # Verify stored hashes match password
+        salt = user['salt']
+        stored_hash = user['password_hash']
+        self.assertTrue(check_passphrase(password, salt, stored_hash), 'Stored hashes should match with password')
+        # Check encryption
+        self.assertIsNone(user.get('personal_info'), 'Personal info should be encrypted')
+        
+        # Check decryption
+        key = get_encryption_key()
+        decrypted_str = decrypt_data(user['encrypted_personal_info'], key)
+        decrypted_data = json.loads(decrypted_str)
+        self.assertEqual(decrypted_data['display_name'], display_name)
+
 
     def test_registration_without_display_name(self):
         email = 'test@test.com'
@@ -48,6 +75,17 @@ class RegistrationHandlerTest(BaseTest):
         self.assertEqual(email, body_2['email'])
         self.assertEqual(email, body_2['displayName'])
 
+        # Check that user is stored in database
+        user = IOLoop.current().run_sync(
+          lambda: self.get_app().db.users.find_one({'email': email})
+          )
+        
+        key = get_encryption_key()
+        decrypted_str = decrypt_data(user['encrypted_personal_info'], key)
+        decrypted_data = json.loads(decrypted_str)
+        self.assertEqual(decrypted_data['display_name'], email)
+        
+
     def test_registration_twice(self):
         body = {
           'email': 'test@test.com',
@@ -60,3 +98,34 @@ class RegistrationHandlerTest(BaseTest):
 
         response_2 = self.fetch('/registration', method='POST', body=dumps(body))
         self.assertEqual(409, response_2.code)
+
+
+    def test_registration_with_extra_info(self):
+        body = {
+          'email': 'test@test.com',
+          'password': 'testPassword',
+          'displayName': 'testDisplayName',
+          'address': 'testAddress',
+          'dateOfBirth': '2026-01-01',
+          'phoneNumber': '1234567890',
+          'listOfDisabilities': ['first','second']
+        }
+      
+        response = self.fetch('/registration', method='POST', body=dumps(body))
+        self.assertEqual(200, response.code)
+        
+        # Check that user is stored in database
+        user = IOLoop.current().run_sync(
+          lambda: self.get_app().db.users.find_one({'email': 'test@test.com'})
+          )
+        
+        key = get_encryption_key()
+        decrypted_str = decrypt_data(user['encrypted_personal_info'], key)
+        decrypted_data = json.loads(decrypted_str)
+        self.assertEqual(decrypted_data['display_name'], 'testDisplayName')
+        self.assertEqual(decrypted_data['address'], 'testAddress')
+        self.assertEqual(decrypted_data['dateOfBirth'], '2026-01-01')
+        self.assertEqual(decrypted_data['phoneNumber'], '1234567890')
+        self.assertEqual(decrypted_data['listOfDisabilities'], ['first','second'])
+        
+        
